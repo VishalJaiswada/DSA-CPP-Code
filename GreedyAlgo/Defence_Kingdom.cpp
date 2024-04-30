6e09f88a5be9d8538b6f3173803d62377892e806
Vishal Jaiswada <vp022570@gmail.com>
2024-04-30 19:56:13 +0530

    Added Defence Kingdom problem

---
+++ GreedyAlgo/Defence_Kingdom.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    cout<<"Enter The Value: "<<endl;
+    int x,y,n;
+    cin>>x>>y>>n;
+    int t=n;
+    vector<int>rowC;
+    vector<int>colC;
+    
+    rowC.push_back(0);
+    colC.push_back(0);
+    while(t--){
+        int a ,b;
+        cin>>a>>b;
+        rowC.push_back(a);
+        colC.push_back(b);
+
+    }
+   
+   //size of 2D grid
+    rowC.push_back(x+1);
+    colC.push_back(y+1);
+   
+   sort(rowC.begin(),rowC.end());
+   sort(colC.begin(),colC.end());
+   
+   int maxLen=INT_MIN;
+   for(int i=1;i<rowC.size();i++){
+        int a=rowC[i-1];
+        int b=rowC[i];
+
+         maxLen=max(maxLen,b-a-1);
+   }
+
+   int maxWidth=INT_MIN;
+   for(int i=1;i<rowC.size();i++){
+        int a=colC[i-1];
+        int b=colC[i];
+    
+        maxWidth=max(maxWidth,b-a-1);
+   }
+    
+    cout<<"Anser is: "<<maxLen*maxWidth<<endl; 
+    return 0;
+}
